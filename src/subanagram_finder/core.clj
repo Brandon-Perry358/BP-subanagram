cce116842bf98b8ec62e9d7beee89a2606242980
Brandon-Perry358 <brandon.perry@mymail.champlain.edu>
2022-04-13 20:04:52 -0400

    Leiningen isn't really working on my machine, so I'm gonna use the github tests to see if this works.

--- src/subanagram_finder/core.clj
+++ src/subanagram_finder/core.clj
@@ -8,12 +8,24 @@
 
 (require '[clojure.string :as str])
 
+(defn determineSubanagram [large small]
+  (def largeOccurance (frequencies (clojure.string/lower-case large)))
+  (def smallOccurance (frequencies (clojure.string/lower-case small)))
+  (def anagramFilter (map #(and (contains? largeOccurance (first %)) 
+  (<= (second %) (get largeOccurance (first %)))) smallOccurance ))
+  (every? true? anagramFilter)
+)
 
-(defn load-dictionary 
+(defn load-dictionaryList 
   "Load list of words from disk."
-  ([] (load-dictionary "resources/words"))
+  ([] (load-dictionaryList "resources/words"))
   ([file-name]
    ;; Code goes here
+   (with-open 
+   [read (clojure.java.io/reader file-name)]
+   (def words (reduce conj [] (line-seq read)))
+   )
+   (reverse (into () words))
   ))
   
 
@@ -23,6 +35,9 @@
   A sub-anagram means it is an anagram of a substring of word."
   [word, word-list]
   ;; Code goes here
+  (def subs (filter #
+  (determineSubanagram word %)word-list)
+  )
 )  
 
 (defn generate-output 
@@ -34,7 +49,11 @@
             \"A At E T Ta a at ate e eat eta t tea\nK O OK k o\"
   "
   [words]
-  ;; Code goes here)
+  ;; Code goes here
+  (let [dictionaryList (load-dictionary)
+  anagram (map #(find-sub-anagrams) % dictionaryList) words]
+  (str/join "\n" (map #(str/join " " %) anagram)))
+  )
   
 
 (defn -main 
@@ -44,4 +63,5 @@
     ;; then
     (println "Usage:\n\tlein run -- word [word ...]")
     ;; else
-    (println (generate-output args))))    
+    (println (generate-output args)))
+    )    
